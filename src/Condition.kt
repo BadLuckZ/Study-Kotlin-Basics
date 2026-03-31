efed500b0b188666adde7b5135bb68cd4e69ffbb
BadLuckZ <thanagorn8802@gmail.com>
2026-04-01 01:50:37 +0700

    chore: condition and loop

---
+++ src/Condition.kt
@@ -0,0 +1,35 @@
+fun main() {
+    val number: Int
+    val checked: Boolean = false
+    val checkedAgain: Boolean = false
+
+    // if-else
+    if (checked) {
+        // "checked" True Condition
+        number = 1
+    } else if (checkedAgain)  {
+        // "checked" False Condition
+        // but "checkedAgain" True Condition
+        number = 2
+    } else {
+        // "checked" and "checkedAgain" False Condition
+        number = 3
+    }
+    println("Current Number is $number")
+
+    // when
+    val obj: Number = 1
+    when (obj) {
+        1 -> println("This is one")
+        2 -> println("This is two")
+        else -> println("This is not 1 and 2")
+    }
+
+    // when as expression
+    val result: String = when (obj) {
+        1 -> "one"
+        2 -> "two"
+        else -> "this is not one and two"
+    }
+    println("Result is $result")
+}
