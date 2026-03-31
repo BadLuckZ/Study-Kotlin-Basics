24a1d257ea116c97894710354d929f310c1899a1
BadLuckZ <thanagorn8802@gmail.com>
2026-04-01 00:22:38 +0700

    chore: type

---
+++ src/Type.kt
@@ -0,0 +1,21 @@
+fun main(){
+    // Byte Short Int Long for "Integers"
+    // Float Double for "Floats"
+    // Boolean for "Booleans"
+    // Char for "Characters"
+    // String for "Strings"
+
+    val int1: Int = 5
+    val float1: Float = 2f
+    val float2: Float = 2.5f
+    val bool1: Boolean = true
+    val char1: Char = 'a'
+    val string1: String = "Hello, World!"
+
+    println("Some Integer: $int1")
+    println("Some Float: $float1")
+    println("Some Float Again: $float2")
+    println("Some Boolean: $bool1")
+    println("Some Char: $char1")
+    println("Some String: $string1")
+}
