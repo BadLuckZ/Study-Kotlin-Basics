280acbe93de2a6535e5c22021ad054a0ee97808f
BadLuckZ <thanagorn8802@gmail.com>
2026-04-01 01:50:59 +0700

    fix: loop

---
+++ src/Loop.kt
@@ -0,0 +1,18 @@
+fun main() {
+    // for loop
+    for (num: Int in 1..5) {
+        println(num)
+    }
+
+    val cakes: List<String> = listOf("Carrot", "Cheese", "Chocolate")
+    for (cake: String in cakes) {
+        println("Cake: $cake")
+    }
+
+    // while loop
+    var cakesEaten: Int = 0
+    while (cakesEaten < 5) {
+        println("Cake Eaten #${cakesEaten+1}")
+        cakesEaten += 1
+    }
+}
